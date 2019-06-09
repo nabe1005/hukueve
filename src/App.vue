e1d9993f3641bea72dd0cfc560b951e05dbb2650
nabe1005 <taiki.w.1113@gmail.com>
2019-06-09 15:01:34 +0900

    リンクの修正

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-brand href="/hukueve">Hukueve</b-navbar-brand>
+        <b-navbar-brand href="/#">Hukueve</b-navbar-brand>
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
